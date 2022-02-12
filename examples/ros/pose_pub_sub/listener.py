2d2a96c858474c709157a8b982f150446b1ac073
nickumia <nickumia6@gmail.com>
2022-02-12 12:01:43 -0500

    lint: python autopep8
    No guarantee that the code is still functional, needs testing haha... examples/ros/pose_pub_sub

--- examples/ros/pose_pub_sub/listener.py
+++ examples/ros/pose_pub_sub/listener.py
@@ -3,11 +3,13 @@
 import rospy
 from turtlesim.msg import Pose
 
+
 def callback(data):
     # rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
     rospy.loginfo('Pose x: %s', data.x)
     rospy.loginfo('Pose y: %s', data.y)
     rospy.loginfo('Pose theta: %s', data.theta)
+
 
 def listener():
 
@@ -21,6 +23,7 @@ def listener():
     rospy.Subscriber('/turtle1/pose', Pose, callback)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
+
 
 if __name__ == '__main__':
     listener()
--- examples/ros/pose_pub_sub/talker.py
+++ examples/ros/pose_pub_sub/talker.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-import rospy 
+import rospy
 import time
-import math 
+import math
 
-from geometry_msgs.msg import Twist, Vector3 # ROS RELATED
-from turtlesim.msg import Pose # ROS and Turtlesim related
+from geometry_msgs.msg import Twist, Vector3  # ROS RELATED
+from turtlesim.msg import Pose  # ROS and Turtlesim related
 
 # leave data here
 # UPDATE THIS FUNCTION WITH YOUR LOGIC TO COMPLETE TASK
+
+
 def callback(data):
     # angle
     # counter clockwise - positive Vector3(0,0,.75)
@@ -19,33 +21,36 @@ def callback(data):
     y_position = data.y
 
     while (y_position <= 9.2):
-        if (theta < math.pi and y_position < 7.50 ):
-            pub.publish(Twist(Vector3(1,0,0),Vector3(0,0,.75)))
+        if (theta < math.pi and y_position < 7.50):
+            pub.publish(Twist(Vector3(1, 0, 0), Vector3(0, 0, .75)))
             break
 
-        elif ( theta < math.pi and y_position < 9.2 ):
-            pub.publish(Twist(Vector3(1,0,0),Vector3(0,0,-.75)))
+        elif (theta < math.pi and y_position < 9.2):
+            pub.publish(Twist(Vector3(1, 0, 0), Vector3(0, 0, -.75)))
             break
+
 
 def listener():
-    # rospy.Subscriber(topic name, the topic data type, function to call once you get the data)
+    # rospy.Subscriber(topic name, the topic data type, function to call once
+    # you get the data)
     rospy.Subscriber('/turtle1/pose', Pose, callback)
 
 
 if __name__ == '__main__':
     try:
-        
+
         # init node
         rospy.init_node('draw_S')
-        
+
         # create publisher
-        # rospy.Publisher(topic, topic data type, how much data to send per time)
+        # rospy.Publisher(topic, topic data type, how much data to send per
+        # time)
         pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1)
-        
+
         # While rospy is not shut down do this
         while not rospy.is_shutdown():
             listener()
-            
+
             # update ros and keep going
             rospy.spin()
 
