cfebed99af13891aaa6131365323f8a416512a4e
nickumia <nickumia6@gmail.com>
2022-02-12 11:58:26 -0500

    lint: python autopep8
    No guarantee that the code is still functional, needs testing haha... examples/opencv

--- examples/opencv/opencv_opencamera.py
+++ examples/opencv/opencv_opencamera.py
@@ -6,13 +6,13 @@ cap = cv2.VideoCapture(0)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    frame = cv2.flip(frame,1)
+    frame = cv2.flip(frame, 1)
 
     # Our operations on the frame come here
     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Display the resulting frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
