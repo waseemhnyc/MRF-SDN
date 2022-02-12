ae3d3ea74cf138d9cb2688c7485b9c569158cd57
nickumia <nickumia6@gmail.com>
2022-02-12 12:18:26 -0500

    lint: python autopep8
    No guarantee that the code is still functional, needs testing haha... something special here? examples/aerial/ros/service_example.py

--- examples/aerial/ros/service_example.py
+++ examples/aerial/ros/service_example.py
@@ -15,12 +15,14 @@ def setMode(mode):
     rospy.wait_for_service('/mavros/set_mode')
     try:
         flightModeService = rospy.ServiceProxy(
-    '/mavros/set_mode', mavros_msgs.srv.SetMode)
+            '/mavros/set_mode', mavros_msgs.srv.SetMode)
         isModeChanged = flightModeService(
-    custom_mode=mode)  # return true or false
+            custom_mode=mode)  # return true or false
         print('%s has been successfully set to %s mode.' % self.name, mode)
-    except rospy.ServiceException, e:
-        print("service set_mode call failed: %s. GUIDED Mode could not be set. Check that GPS is enabled" % e)
+    except rospy.ServiceException as e:
+        print(
+            "service set_mode call failed: %s. GUIDED Mode could not be set. Check that GPS is enabled" %
+            e)
 
 
 def arm():
@@ -28,11 +30,11 @@ def arm():
     try:
         print('Warning! %s is arming! ' % self.name)
         armService = rospy.ServiceProxy(
-    '/mavros/cmd/arming',
-     mavros_msgs.srv.CommandBool)
+            '/mavros/cmd/arming',
+            mavros_msgs.srv.CommandBool)
         armService(True)
         print('---> %s has armed.' % self.name)
-    except rospy.ServiceException, e:
+    except rospy.ServiceException as e:
         print("Service arm call failed: %s" % e)
 
 
@@ -41,11 +43,11 @@ def disarm():
     try:
         print('Warning! %s is disarming! ' % self.name)
         armService = rospy.ServiceProxy(
-    '/mavros/cmd/arming',
-     mavros_msgs.srv.CommandBool)
+            '/mavros/cmd/arming',
+            mavros_msgs.srv.CommandBool)
         armService(False)
         print('---> %s has disarmed.' % self.name)
-    except rospy.ServiceException, e:
+    except rospy.ServiceException as e:
         print("Service arm call failed: %s" % e)
 
 
@@ -54,26 +56,26 @@ def takeoffMode():
     try:
         print('Warning! %s is in take off mode' % self.name)
         takeoffService = rospy.ServiceProxy(
-    '/mavros/cmd/takeoff', mavros_msgs.srv.CommandTOL)
+            '/mavros/cmd/takeoff', mavros_msgs.srv.CommandTOL)
         takeoffService(altitude=0, latitude=0, longitude=0, min_pitch=0, yaw=0)
-    except rospy.ServiceException, e:
+    except rospy.ServiceException as e:
         print("Service takeoff call failed: %s" % e)
 
 
 def landMode():
     rospy.wait_for_service('/mavros/cmd/land')
-       try:
-            print('Warning! %s is in landing mode' % self.name)
-            landService = rospy.ServiceProxy(
-    '/mavros/cmd/land', mavros_msgs.srv.CommandTOL)
-            isLanding = landService(
-    altitude=0,
-    latitude=0,
-    longitude=0,
-    min_pitch=0,
-     yaw=0)
-        except rospy.ServiceException, e:
-            print("service land call failed: %s. The vehicle cannot land " %e)
+    try:
+        print('Warning! %s is in landing mode' % self.name)
+        landService = rospy.ServiceProxy(
+            '/mavros/cmd/land', mavros_msgs.srv.CommandTOL)
+        isLanding = landService(
+            altitude=0,
+            latitude=0,
+            longitude=0,
+            min_pitch=0,
+            yaw=0)
+    except rospy.ServiceException as e:
+        print("service land call failed: %s. The vehicle cannot land " % e)
 
 
 if __name__ == '__main__':
